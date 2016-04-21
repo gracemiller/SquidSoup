b99784ce0b0cae02ab6366b4464ee89ea4a22004
Connor Ferry <connorferry8@me.com>
2016-04-21 20:56:21 +0100

    Navigation pages / styling
    Navigation pages / styling

--- wordpress/wp-content/themes/squid-soup/footer.php
+++ wordpress/wp-content/themes/squid-soup/footer.php
@@ -11,7 +11,6 @@
     <footer itemscope itemtype="http://schema.org/WPFooter" id="footer" role="contentinfo" class = "footer grey-bg">
 
         <div class="container">
-
                 
 				<div style="width:60%; display:inline-block">
                     <div style="display:inline-block; margin-right:30px">
@@ -27,17 +26,16 @@
                         <p>Call us today to find out <br> how we can help.</p>
                     </div>
                     <hr>
-                    <p>All rights reserved 2016. OCEAN OF LIGHT is a registered trademark.</p>
+                    <p>© All rights reserved 2016</p>
                 </div>
                 
                 <div style="width:30%; display:inline-block; float:right">
-                    <img src="images/circle.png" style="width:50px">
-                    <img src="images/circle.png" style="width:50px">
-                    <img src="images/circle.png" style="width:50px">
+                    <img src="wp-content/themes/squid-soup/images/circle.png" style="width:50px">
+                    <img src="wp-content/themes/squid-soup/images/circle.png" style="width:50px">
+                    <img src="wp-content/themes/squid-soup/images/circle.png" style="width:50px">
                     <h4 style="font-weight:100"><strong>0845 604 0571</strong></h4>
                     <h4 style="font-weight:100"><strong>email@squidsoup.co.uk</strong></h4>
                 </div>
-	            
 
 	    </div><!-- container -->
 
